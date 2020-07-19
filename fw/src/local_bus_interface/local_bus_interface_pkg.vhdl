60711c938538f5e32eb837503ab38c07d80fefca
Nickolas Upole <nickolas@uchicago.edu>
2020-07-18 20:15:39 -0500

    #7: Add local_bus_interface

---
+++ fw/src/local_bus_interface/local_bus_interface_pkg.vhdl
@@ -0,0 +1,9 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+library work;
+use work.types_pkg.all;
+
+package local_bus_interface_pkg is
+    alias registers is array_std_logic_vector;
+end package;
